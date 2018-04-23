08399442bb65e61462d9cc55a72f786439c2356e
sy <somarl@live.com>
2018-04-23 19:53:58 +0800

    feat(snippets): axis and static

---
+++ snippets/axis.js
@@ -0,0 +1,87 @@
+{   // AXIS
+    getAxis()
+    // getAxis(1, 1, pilot)
+
+    function globalAxis (size) {
+        function makeTextPlane (text, color, size) {
+            const dynamicTexture = new BABYLON.DynamicTexture('DynamicTexture', 50, scene, true)
+            dynamicTexture.hasAlpha = true
+            dynamicTexture.drawText(text, 5, 40, 'bold 36px Arial', color , 'transparent', true)
+            const plane = new BABYLON.Mesh.CreatePlane('TextPlane', size, scene, true)
+            plane.material = new BABYLON.StandardMaterial('TextPlaneMaterial', scene)
+            plane.material.backFaceCulling = false
+            plane.material.specularColor = new BABYLON.Color3(0, 0, 0)
+            plane.material.diffuseTexture = dynamicTexture
+            return plane
+        }
+
+        const axisX = BABYLON.Mesh.CreateLines('axisX', [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0), 
+            new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+        ], scene)
+        axisX.color = new BABYLON.Color3(1, 0, 0)
+        const xChar = makeTextPlane('X', 'red', size / 10)
+        xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0)
+
+        const axisY = BABYLON.Mesh.CreateLines('axisY', [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3( -0.05 * size, size * 0.95, 0), 
+            new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3( 0.05 * size, size * 0.95, 0)
+        ], scene)
+        axisY.color = new BABYLON.Color3(0, 1, 0)
+        const yChar = makeTextPlane('Y', 'green', size / 10)
+        yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size)
+
+        const axisZ = BABYLON.Mesh.CreateLines('axisZ', [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0 , -0.05 * size, size * 0.95),
+            new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0, 0.05 * size, size * 0.95)
+            ], scene)
+        axisZ.color = new BABYLON.Color3(0, 0, 1)
+        var zChar = makeTextPlane('Z', 'blue', size / 10)
+        zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size)
+    }
+
+    function localAxis(size) {
+        const localAxisX = BABYLON.Mesh.CreateLines('localAxisX', [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0), 
+            new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+        ], scene)
+        localAxisX.color = new BABYLON.Color3(1, 0, 0)
+
+        localAxisY = BABYLON.Mesh.CreateLines('localAxisY', [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
+            new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
+        ], scene)
+        localAxisY.color = new BABYLON.Color3(0, 1, 0)
+
+        const localAxisZ = BABYLON.Mesh.CreateLines('localAxisZ', [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0 , -0.05 * size, size * 0.95),
+            new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0, 0.05 * size, size * 0.95)
+        ], scene)
+        localAxisZ.color = new BABYLON.Color3(0, 0, 1)
+
+        const localAxisZM = BABYLON.Mesh.CreateLines('localAxisZ', [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, -15)
+        ], scene)
+        localAxisZ.color = new BABYLON.Color3(0, 0, 1)
+
+        const localOrigin = BABYLON.MeshBuilder.CreateBox('localOrigin', {size:1}, scene)
+        localOrigin.isVisible = false
+
+        localAxisX.parent = localOrigin
+        localAxisY.parent = localOrigin
+        localAxisZ.parent = localOrigin
+        localAxisZM.parent = localOrigin
+
+        return localOrigin
+    }
+
+    function getAxis (needGlobalAxis=true, needLocalAxis=false, origin=false, globalSize=8, localSize=2) {
+        if (needGlobalAxis) {
+            globalAxis(globalSize)
+        }
+        if (needLocalAxis && parent) {
+            const localOrigin = localAxis(localSize)
+            localOrigin.parent = origin
+        }
+    }
+}
---
+++ snippets/static.js
@@ -0,0 +1,21 @@
+{   // static
+    // macro
+    var V3 = BABYLON.Vector3
+    var C4 = BABYLON.Color4
+    var X = BABYLON.Axis.X
+    var Y = BABYLON.Axis.Y
+    var Z = BABYLON.Axis.Z
+    var LOCAL = BABYLON.Space.LOCAL
+    var WORLD = BABYLON.Space.WORLD
+    var PI = Math.PI
+    // scene
+    var scene = new BABYLON.Scene(engine)
+    scene.clearColor = new BABYLON.Color3( .5, .5, .5)
+    // camera
+    var camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 0, new BABYLON.Vector3(3, 3, -3), scene)
+    camera.setPosition(new BABYLON.Vector3(10, 5, -20))
+    camera.attachControl(canvas, true)
+    // lights
+    var light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(1, 0.5, 0), scene)
+    light.intensity = 0.8
+}
