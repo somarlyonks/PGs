0c4cc05e11de2c561fee3c50ec4a9a1569cae8f9
Sy <somarl@live.com>
2018-04-24 11:29:21 +0800

    feat(snippets): add canvas fix function

---
+++ snippets/utils.js
@@ -0,0 +1,11 @@
+// {  // utils
+    function getCanvasFixWH (basicSize=1) {
+        const ratio = canvas.height / canvas.width
+        const fix = 1 / Math.sqrt(ratio)
+
+        const canvasFixW = basicSize * fix
+        const canvasFixH = basicSize * fix * ratio
+
+        return {canvasFixW, canvasFixH}
+    }
+// }
