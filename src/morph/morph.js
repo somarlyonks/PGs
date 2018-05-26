950fbb6f4ed3ddd1e04ef5980c7b17dd41ea7522
sy <somarl@live.com>
2018-05-26 20:06:37 +0800

    feat(morph): trailing

--- src/morph/morph.js
+++ src/morph/morph.js
@@ -1,4 +1,5 @@
 var createScene = function() {
+    var V3 = BABYLON.Vector3
     var scene = new BABYLON.Scene(engine)
     scene.clearColor = new BABYLON.Color3( .5, .5, .5)
     var camera = new BABYLON.ArcRotateCamera('camera1',  0, 0, 0, new BABYLON.Vector3(0, 0, -0), scene)
@@ -7,78 +8,89 @@ var createScene = function() {
     var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(1, 0.5, 0), scene)
     light.intensity = 0.7
     var pl = new BABYLON.PointLight('pl', new BABYLON.Vector3(0, 0, 0), scene)
-    pl.diffuse = new BABYLON.Color3(1, 1, 1)
+    // pl.diffuse = new BABYLON.Color3(1, 1, 1)
     pl.intensity = 0.95
+
     var mat = new BABYLON.StandardMaterial('mat1', scene)
-    var txtr = new BABYLON.Texture('https://s2.d2scdn.com/2018/05/25/Fi2nVbbp37i9kOc5e27qaiatZuVi.png')
-    //   txtr.hasAlpha = true
-    //   mat.alphaMode = 1
-    mat.diffuseTexture = txtr
+    mat.alpha = 1.0
+    mat.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5)
+    mat.backFaceCulling = false
+    var txtr = new BABYLON.Texture('https://s2.d2scdn.com/2018/05/26/Fs-lwjlP3YWYkqclI1ZfK-jSd8B8.png')
+    txtr.wrapU = BABYLON.Texture.CLAMP_ADDRESSMODE
+    txtr.wrapV = BABYLON.Texture.CLAMP_ADDRESSMODE
     mat.opacityTexture = txtr
     mat.specularColor = new BABYLON.Color3(0, 0, 0)
     mat.emissiveColor = new BABYLON.Color3(0, 0, 0)
-    mat.diffuseColor = new BABYLON.Color3(0, 0, 0)
+    // mat.wireframe = true;
 
-    mat.alpha = 1
-    mat.diffuseColor = new BABYLON.Color3(0.5, 0.5, 1.0)
-    mat.backFaceCulling = false
-    //   mat.wireframe = true
-
-    // path function
-    var pathFunction = function(k) {
-        var path = [] 
-        for (var i = 0; i < 60; i++) {
-            var x =  i - 30
-            var y = 0
-            var z = k
-            path.push(new BABYLON.Vector3(x, y, z))
-        }
-        return path
+    // tube creation
+    // var sideOrientation = BABYLON.Mesh.FRONTSIDE
+    var radius = 2
+    var radiusFix = 1
+    var path = []
+    var ptCount = 100
+    var maxLength = 15
+    var ptDistance = maxLength / ptCount
+    for(var i = 0; i < ptCount; i++) {
+        var x = 0.000001 * i
+        var y = 0
+        var z = 0
+        path.push(new V3(x, y, z))
     }
 
-    // update path function
-    var updatePath = function(path, k) {
-        for (var i = 0; i < path.length; i++) {
-            var x = path[i].x
-            var z = path[i].z
-            var y = 20 * Math.sin(i/ 10) * Math.sin(k + z / 40)  
-            path[i].x = x
-            path[i].y = y
-            path[i].z = z
-        }
-    }
+    var radiusFunction = (i/* , distance */) => radius - (radius - radiusFix) / path.length * i
 
-    // ribbon creation
-    var sideO = BABYLON.Mesh.BACKSIDE
-    var pathArray = []
-    for(var i = -20; i < 20; i++) {
-        pathArray.push(pathFunction(i * 2))
-    }
-    console.log('path', pathArray)
-    var mesh = BABYLON.Mesh.CreateRibbon('ribbon', pathArray, false, false, 0, scene, true, sideO)
+    var mesh = BABYLON.MeshBuilder.CreateTube('tube', {
+        path, radius, radiusFunction,
+        sideOrientation: BABYLON.Mesh.BACKSIDE,
+        updatable: true,
+    }, scene)
     mesh.material = mat
 
-    //   mesh.scaling.x = 10
-    console.log(mesh.scaling)
+    const subject = BABYLON.MeshBuilder.CreateBox('subject', {size: 4}, scene)
+    let clock = 0
+    subject.registerAfterWorldMatrixUpdate(trail)
+    scene.registerAfterRender(cutTail)
+
+    function trail () {
+        const latestPosition = path[0]
+        if (latestPosition && V3.Distance(latestPosition, subject.position) < ptDistance) {
+            return
+        }
+
+        path.pop()
+        path.unshift(subject.position)
+
+        // mesh = BABYLON.MeshBuilder.CreateTube(null, {
+        //     path,
+        //     radiusFunction,
+        //     instance: mesh
+        // })
+    }
+
+    function cutTail () {
+        clock++
+        if (clock % 5 === 0) {
+            path.pop()
+            path.unshift(subject.position)
+        }
+        mesh = BABYLON.MeshBuilder.CreateTube(null, {
+            path,
+            radiusFunction,
+            instance: mesh
+        })
+    }
+
 
     // morphing
-    var k = 0
-    scene.registerBeforeRender(function(){
-        // update pathArray
-        for(var p = 0; p < pathArray.length; p++) {
-            updatePath(pathArray[p], k)
-        }
-        // ribbon update
-        mesh = BABYLON.Mesh.CreateRibbon(null, pathArray, null, null, null, null, null, null, mesh)
-        k += 0.05
-        pl.position = camera.position
-    })
+    function move () {
+        subject.rotation.z += 0.01
+        subject.translate(new V3(-1, 0, 0), 0.1, BABYLON.Space.LOCAL)
+    }
 
-    //   setTimeout(() => {
-    //       scene.registerAfterRender(() => {
-    //           console.log('registered')
-    //       })
-    //   }, 1000)
+    scene.registerAfterRender(move)
+
+    // setTimeout(() => scene.unregisterAfterRender(move), 10000)
 
     return scene
 }
