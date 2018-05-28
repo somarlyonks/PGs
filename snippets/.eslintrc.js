515d467a8f5401d2ae723fd929d349019dee8c81
sy <somarl@live.com>
2018-05-28 18:01:39 +0800

    fix(utils): fix eslint

---
+++ snippets/.eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    // {off: 0, warn: 1, error: 2}
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-unused-vars": [
+            "off",
+            {
+                "vars": "all",
+                "args": "none",
+                "ignoreRestSiblings": false
+            }
+        ],
+        "no-console": 0,
+        "curly": 0,
+        "eqeqeq": "error",
+        "no-inner-declarations": 0,
+    },
+    "globals": {
+        "BABYLON": true,
+        "engine": true, // PG.engine
+        "canvas": true,
+        "scene": true,
+    }
+};
