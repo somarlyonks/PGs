4c1128e71e949fdfe698c8e94b654f149b767bc1
sy <somarl@live.com>
2018-05-30 17:14:39 +0800

    fix(axis): add backward support

--- snippets/axis.js
+++ snippets/axis.js
@@ -12,20 +12,32 @@
         }
     }
 
-    function axis3 (size, nameList) {
+    function axis3 (size, nameList, back) {
+        const V3 = BABYLON.Vector3
+        const anchorSize = 0.05 * size
+
         const axisX = BABYLON.Mesh.CreateLines(nameList[0], [
-            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
-            new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+            back ? new V3(-size, 0, 0) : V3.Zero(),
+            new V3(size, 0, 0),
+            new V3(size * 0.95, anchorSize, 0),
+            new V3(size, 0, 0),
+            new V3(size * 0.95, -anchorSize, 0)
         ], scene)
         axisX.color = new BABYLON.Color3(1, 0, 0)
         const axisY = BABYLON.Mesh.CreateLines(nameList[1], [
-            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
-            new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
+            back ? new V3(0, -size, 0) : V3.Zero(),
+            new V3(0, size, 0),
+            new V3(-anchorSize, size * 0.95, 0),
+            new V3(0, size, 0),
+            new V3(anchorSize, size * 0.95, 0)
         ], scene)
         axisY.color = new BABYLON.Color3(0, 1, 0)
         const axisZ = BABYLON.Mesh.CreateLines(nameList[2], [
-            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95),
-            new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)
+            back ? new V3(0, 0, -size) : V3.Zero(),
+            new V3(0, 0, size),
+            new V3(0, -anchorSize, size * 0.95),
+            new V3(0, 0, size),
+            new V3(0, anchorSize, size * 0.95)
         ], scene)
         axisZ.color = new BABYLON.Color3(0, 0, 1)
 
